eb0b4e5ee2f2424542b316c279fe414a38600569
AbrarAbe <abrar.abe01@gmail.com>
2025-05-10 18:19:59 +0700

    feat(android): Add necessary Gradle and Android setup for flutter_local_notification package
    Added the `USE_EXACT_ALARM`, `RECEIVE_BOOT_COMPLETED`, and `USE_FULL_SCREEN_INTENT` permissions to `AndroidManifest.xml`. These permissions are required to schedule exact alarms, receive notifications after device boot, and potentially display full-screen notifications for the timer feature.
    
    Enabled `multiDexEnabled` and `coreLibraryDesugaring` in `build.gradle.kts`. This is necessary to support potentially larger app sizes due to added dependencies (e.g., notification/alarm libraries) and to utilize newer Java APIs on older Android versions.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,6 +11,9 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
+        // Flag to enable support for the new language APIs
+        isCoreLibraryDesugaringEnabled = true
+        // Sets Java compatibility to Java 11
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -20,6 +23,7 @@ android {
     }
 
     defaultConfig {
+        multiDexEnabled = true
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.takoapps.pomothing"
         // You can update the following values to match your application needs.
@@ -37,6 +41,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+   coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
